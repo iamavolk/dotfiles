25e7c768fcda26d669ad2dee51ad8915165c6e94
avr <alexfromshermanoaks@berkeley.edu>
2023-12-24 23:47:14 -0800

    a little moar prompt real estate (no hostname)

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,14 +6,15 @@ function fish_prompt
     if not set -q VIRTUAL_ENV_DISABLE_PROMPT
         set -g VIRTUAL_ENV_DISABLE_PROMPT true
     end
-    set_color yellow
+    set_color magenta
     printf '%s' $USER
     set_color normal
-    printf ' at '
 
-    set_color magenta
-    echo -n (prompt_hostname)
-    set_color normal
+    #printf ' at '
+    #set_color magenta
+    #  echo -n (prompt_hostname)
+    #  set_color normal
+
     printf ' in '
 
     set_color $fish_color_cwd
