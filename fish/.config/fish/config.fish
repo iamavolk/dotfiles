8b784d0a086944cc2c803689e3e50474d91abf51
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2025-11-07 03:48:13 -0800

    refactor(fish): clean up config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,24 +1,25 @@
 alias l='eza -lbS --git --group-directories-first'
 alias la='l -a'
 alias ll='eza -aablGS --group-directories-first'
-alias lt='eza -blT --no-user --no-time --no-permissions --group-directories-first'
 alias ls='eza -a --group-directories-first'
+alias lt='eza -blT \
+    --no-user \
+    --no-time \
+    --no-permissions \
+    --group-directories-first'
 
-alias gs='git status'
 alias ga='git add'
 alias gc='git commit'
 alias gd='git diff'
+alias gs='git status'
 
-alias avim='nvim -n -u NONE -i NONE -N -c "set noswapfile nobackup nowritebackup noundofile"'
 alias vi='nvim'
 alias iv='NVIM_APPNAME=nvim-12 nvim'
+alias avim='nvim -n -u NONE -i NONE \
+    -N -c "set noswapfile nobackup nowritebackup noundofile"'
 
 set fish_prompt_pwd_dir_length 0
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    fastfetch
 end
-
-set -x GOOGLE_CLOUD_PROJECT "gemini-cli-test0"
-
-fastfetch
