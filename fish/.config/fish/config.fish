b30331b9a434777b32d6257713201c702f03141a
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-02-09 05:15:06 -0800

    feat(fish): variuos incremental improvements
    tight yazi integration; removal of the unused bob
    shim sourcing; etc.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,10 @@
+if status is-interactive
+    fastfetch
+end
+set fish_prompt_pwd_dir_length 0
+
 alias l='eza -lbS --git --group-directories-first'
-alias la='l -a'
+alias la='l -ag'
 alias ll='eza -aablGS --group-directories-first'
 alias ls='eza -a --group-directories-first'
 alias lt='eza -blT \
@@ -18,14 +23,21 @@ alias iv='NVIM_APPNAME=nvim-12 nvim'
 alias avim='nvim -n -u NONE -i NONE \
     -N -c "set noswapfile nobackup nowritebackup noundofile"'
 
-set fish_prompt_pwd_dir_length 0
-
-if status is-interactive
-    fastfetch
+# yazi + fish cwd sync integration
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 bind alt-l 'commandline -r "l"; commandline -f execute'
+bind ctrl-y 'commandline y; commandline -f execute'
 
 zoxide init --cmd cd fish | source
 fzf --fish | source
-source $HOME/.local/share/bob/env/env.fish
+# source $HOME/.local/share/bob/env/env.fish
+
+alias vitest='NVIM_APPNAME=nvim-test nvim'
